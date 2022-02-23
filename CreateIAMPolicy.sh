7817970acd0704f6e5637d78d760fd1336989583
Takenori Kusaka <kokorokagami@gmail.com>
2022-02-23 17:05:06 +0900

    add example

--- CreateIAMPolicy.sh
+++ CreateIAMPolicy.sh
@@ -11,21 +11,21 @@ echo "Start to create IAM Policy files"
 echo "Export dir: IAMPoliciyFiles"
 DIRPATH="./IAMPoliciyFiles/"
 mkdir -p $DIRPATH
-find . -name "*.json" | while read -r fname
+find . -name "./CFn/*.json" | while read -r fname
 do
   echo "Find: $fname"
   echo "Export: $DIRPATH`basename $fname`-IAMPolicy.json"
   cfnlp -i $fname | tee $DIRPATH`basename $fname`-IAMPolicy.json
 done
 
-find . -name "*.yaml" | while read -r fname
+find . -name "./CFn/*.yaml" | while read -r fname
 do
   echo "Find: $fname"
   echo "Export: $DIRPATH`basename $fname`-IAMPolicy.json"
   cfnlp -i $fname | tee $DIRPATH`basename $fname`-IAMPolicy.json
 done
 
-find . -name "*.yml" | while read -r fname
+find . -name "./CFn/*.yml" | while read -r fname
 do
   echo "Find: $fname"
   echo "Export: $DIRPATH`basename $fname`-IAMPolicy.json"
