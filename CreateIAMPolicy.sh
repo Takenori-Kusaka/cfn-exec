4d10e035b7ec68ff07d4f32a933bcdc136bebba1
Takenori Kusaka <kokorokagami@gmail.com>
2022-02-23 16:51:03 +0900

    Update CreateIAMPolicy.sh

--- CreateIAMPolicy.sh
+++ CreateIAMPolicy.sh
@@ -10,20 +10,24 @@ pip install cfnlp
 echo "Start to create IAM Policy files"
 echo "Export dir: IAMPoliciyFiles"
 dirpath = "./IAMPoliciyFiles/"
+
+find . -name "*.json" | while read -r fname
+do
+  echo "Find: $fname"
+  echo "Export: $dirpath`basename $fname`-IAMPolicy.json"
+  cfnlp -i $fname | tee $dirpath`basename $fname`-IAMPolicy.json
+done
+
 find . -name "*.yaml" | while read -r fname
 do
   echo "Find: $fname"
+  echo "Export: $dirpath`basename $fname`-IAMPolicy.json"
   cfnlp -i $fname | tee $dirpath`basename $fname`-IAMPolicy.json
 done
 
 find . -name "*.yml" | while read -r fname
 do
   echo "Find: $fname"
-  cfnlp -i $fname | tee $dirpath`basename $fname`-IAMPolicy.json
-done
-
-find . -name "*.json" | while read -r fname
-do
-  echo "Find: $fname"
+  echo "Export: $dirpath`basename $fname`-IAMPolicy.json"
   cfnlp -i $fname | tee $dirpath`basename $fname`-IAMPolicy.json
 done
