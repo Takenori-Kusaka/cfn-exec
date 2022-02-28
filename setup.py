ee551b5d87755532ccd5361139e46b4f8cbc63a5
Takenori Kusaka <kokorokagami@gmail.com>
2022-02-28 22:35:12 +0900

    fix

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup (
     author_email='takenori.kusaka@gmail.com',
     url='https://github.com/Takenori-Kusaka/cfn-giam',
     license='MIT',
-    packages=find_packages("cfngiam"),
+    packages=["cfngiam"],
     zip_safe=True,
     keywords='aws',
     install_requires=[
